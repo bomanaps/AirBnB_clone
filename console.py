bc838e7fe61dbf47a46d17b5ea2bccb3826f63d9
arapBii <105924200+Kevin7744@users.noreply.github.com>
2023-12-07 10:48:26 +0300

    Update console.py
    Command interpreter skeleton structure

--- console.py
+++ console.py
@@ -20,44 +20,49 @@ class HBNBCommand(cmd.Cmd):
   l_classes = ['BaseModel', 'User', 'Amenity', 'Place', 'City', 'State', 'Review']
   l_c = ['create', 'show', 'update', 'all', 'destroy', 'count']
 
-def precmd(self, arg):
-  """ Parses the command input """
-  if '.' in arg and '(' in arg and ')' in arg:
-    cls = arg.split('.')
-    cnd = cls[1].split('(')
-    args = cnd[1].split(')')
-    if cls[0] in HBNBCommand.l_classes and cnd[0] in HBNBCommand.l_c:
-      arg = cnd[0] + ' ' + cls[0] + ' ' + arg[0]
-    return arg
+  def precmd(self, arg):
+    """ Parses the command input """
+    if '.' in arg and '(' in arg and ')' in arg:
+      cls = arg.split('.')
+      cnd = cls[1].split('(')
+      args = cnd[1].split(')')
+      if cls[0] in HBNBCommand.l_classes and cnd[0] in HBNBCommand.l_c:
+        arg = cnd[0] + ' ' + cls[0] + ' ' + arg[0]
+      return arg
+  
+  def help_help(self):
+    """ The help command description """
+    print("Provide description of a given command")
+  
+  # def emptyline(self):
+  #   """ do nothing empty line """
+  #   pass
+  
+  def do_count(self, cls_name):
+    """ Counts number of instances """
+  
+  def do_create(self, type_model):
+    """ Creates an instance according to a given class """
+  
+  def do_show(self, arg):
+    """ Show string representaion of an instance passed """
+  
+  def do_destroy(self, arg):
+    """ Delete an instance passed """
+  
+  def do_all(self, arg):
+    """ Prints string reprentation of all instances of a given class """
+  
+  def do_update(self, arg):
+    """ Update an instance based on the class name and id """
+  
+  def do_quit(self, line):
+    """ Quit command to exit intrepeter """
+    return True
+  
+  def do_EOF(self, line):
+    """ EOF command to exit intepreter """
+    return True
 
-def help_help(self):
-  """ The help command description """
-  print("Provide description of a given command")
 
-# def emptyline(self):
-#   """ do nothing empty line """
-#   pass
 
-def do_count(self, cls_name):
-  """ Counts number of instances """
-
-def do_create(self, type_model):
-  """ Creates an instance according to a given class """
-
-def do_show(self, arg):
-  """ Show string representaion of an instance passed """
-
-def do_destroy(self, arg):
-  """ Delete an instance passed """
-
-def do_all(self, arg):
-  """ Prints string reprentation of all instances of a given class """
-
-def do_update(self, arg):
-  """ Update an instance based on the class name and id """
-
-def do_quit(self, line):
-  """ Quit command to exit intrepeter """
-  return True
-
-def do_EOF(self, )
