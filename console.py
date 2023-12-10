2e8f5aede086b9330b76e68cf3fbb88245f437a5
arapBii <105924200+Kevin7744@users.noreply.github.com>
2023-12-10 11:42:11 +0300

    Update console.py

--- console.py
+++ console.py
@@ -84,13 +84,71 @@ class HBNBCommand(cmd.Cmd):
 
     def do_destroy(self, arg):
         """ Delete an instance passed """
+        if not arg:
+            print(" *** Class name missing *** ")
+            return
+        args = arg.split(' ')
+        if args[0] not in HBNBCommand.list_classes:
+            print("*** Class doesn't exist *** """)
+        elif len(args) == 1:
+            print(" *** Instance id missing ***")
+        else:
+            all_objs = storage.all()
+            for key, value in all_objects.items():
+                object_name = value.__class__.__name__
+                object_id = value.id
+                if object_name == args[0] and object_id == args[1].strip('"'):
+                    del value
+                    del storage._FileStorage__objects[key]
+                    storage.save()
+                    reurn
+                print("*** No instance found ***")
         
     def do_all(self, arg):
         """ Prints string reprentation of all instances of a given class """
-  
+        if not arg:
+            print("*** Class name missing ***")
+            return
+        args = arg.split(' ')
+        if args[0] not in HBNBCommand.list_classes:
+            print("*** Class doesn't exist ***")
+        else:
+            all_objects = storage.all()
+            list_instances = []
+            for key, value in all_objects.items():
+                object_name = value.__class__.__name__
+                if object_name == args[0]:
+                    list_instances += [value.__str__]
+                print(list_instances)
+
     def do_update(self, arg):
         """ Update an instance based on the class name and id """
-  
+        if not arg:
+            print("*** Class name missing ***")
+            return
+        a = ""
+        for argv in arg.split(','):
+            a = a + argv
+        args = shlex.split(a)
+        if args[0] not in HBNBCommand.list_classes:
+            print(" *** Class doesn't exist *** ")
+        elif len(args) == 1:
+            print(" *** instance id missing ***")
+        else:
+            all_objects = storage.all()
+            for key, object in all_objects.items():
+                object_name = object.__class__.__name__
+                object_id = object.id
+                if object_name == args[0] and object_id == args[1].strip('"'):
+                    if len(args) == 2:
+                        print("*** attribute name missing*** ")
+                    elif len(args) == 3:
+                        print(" *** Value missing *** ")
+                    else:
+                        setattr(object, args[2], args[3])
+                        storage.save()
+                print(" *** No instance found ***")
+
     def do_quit(self, line):
         """ Quit command to exit intrepeter """
         return True
@@ -99,5 +157,5 @@ class HBNBCommand(cmd.Cmd):
         """ EOF command to exit intepreter """
         return True
 
-
-
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
