24f2810a048171a20abf25c6de2e37b65317695f
Kevin7744 <arapbiisubmisions@gmail.com>
2023-12-11 10:27:55 +0300

    PyCode style fix

--- models/user.py
+++ models/user.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
     """User class that inherits from BaseModel"""
     email = ""
     password = ""
     first_name = ""
     last_name = ""
-
